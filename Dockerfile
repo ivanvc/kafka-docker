ac70eccc8e212bcc1c7fb8d0b268f2649f52cd9a
Ivan Valdes <iv@nvald.es>
2014-05-13 16:56:32 +0000

    Support multiple zk hosts

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-FROM ubuntu
+FROM dockerfile/java
 
-MAINTAINER Wurstmeister 
-RUN echo "deb http://archive.ubuntu.com/ubuntu precise universe" >> /etc/apt/sources.list; apt-get update; apt-get install -y unzip  openjdk-6-jdk wget git
+MAINTAINER Ivan Valdes <ivan@vald.es>
 
 RUN wget -q http://mirror.gopotato.co.uk/apache/kafka/0.8.1.1/kafka_2.8.0-0.8.1.1.tgz -O /tmp/kafka_2.8.0-0.8.1.1.tgz
 RUN tar xfz /tmp/kafka_2.8.0-0.8.1.1.tgz -C /opt
 
-ENV KAFKA_HOME /opt/kafka_2.8.0-0.8.1
+ENV KAFKA_HOME /opt/kafka_2.8.0-0.8.1.1
 ADD start-kafka.sh /usr/bin/start-kafka.sh 
-CMD start-kafka.sh 
+ENTRYPOINT /usr/bin/start-kafka.sh 
--- start-kafka.sh
+++ start-kafka.sh
@@ -1,4 +1,20 @@
-sed -r -i "s/(zookeeper.connect)=(.*)/\1=$ZK_PORT_2181_TCP_ADDR/g" $KAFKA_HOME/config/server.properties
+#!/bin/bash
+
+zk_hosts=
+[[ -v ZK_PORT_2181_TCP_ADDR ]] && zk_hosts=$ZK_PORT_2181_TCP_ADDR 
+
+i=1
+while [ -v ZK_${i}_PORT_2181_TCP_ADDR ] ; do
+  zk_host=$(eval echo "\$ZK_${i}_PORT_2181_TCP_ADDR")
+  if [ -z "$zk_hosts" ] ; then
+    zk_hosts=$zk_host
+  else
+    zk_hosts=$zk_hosts,$zk_host
+  fi
+  i=$(expr $i + 1)
+done
+
+sed -r -i "s/(zookeeper.connect)=(.*)/\1=$zk_hosts/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/(broker.id)=(.*)/\1=$BROKER_ID/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/#(advertised.host.name)=(.*)/\1=$HOST_IP/g" $KAFKA_HOME/config/server.properties
 sed -r -i "s/^(port)=(.*)/\1=$PORT/g" $KAFKA_HOME/config/server.properties
